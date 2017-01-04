c388e6cbd26e6a360a8d39783804225fe82dde79
Nasim Damirchli <nasim.damirchli89@gmail.com>
2017-01-04 14:38:23 +0330

    api

--- ChromeExtension/main.py
+++ ChromeExtension/main.py
@@ -1,15 +1,18 @@
+from django.utils import timezone
+from shortenersite.views import shorten
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework import serializers
+import datetime
 
-from rss.models import News
+from entities.models import Entity
 from rss.elastic import news_with_terms
+from rss.models import News, TelegramPost
 from entities.tasks import get_user_entity
 from newsbot.settings import NEWS_PER_PAGE
-from django.utils import timezone
-from shortenersite.views import shorten
 from telegrambot.news_template import news_image_page
 
 
@@ -57,3 +60,60 @@ def get_user_news(username):
                 }
         response.append(news)
     return response
+
+
+@api_view(['GET'])
+@authentication_classes((TokenAuthentication,))
+@permission_classes((IsAuthenticated,))
+def get_telegram_codal(request):
+    return Response(get_one_tg('کدال'))
+
+
+def get_one_tg(name):
+    posts = News.objects.filter(base_news__news_agency__fa_name=name,
+                                base_news__published_date__gte=timezone.now() - datetime.timedelta(days=10)).order_by(
+        '-base_news__published_date')[:50]
+    response = []
+    for post in posts:
+        response.append(news_serializer(post))
+    return response
+
+
+def news_serializer(news):
+    return ({'title': news.base_news.title,
+             'news': news.body,
+             'photo': news_image_page(news),
+             'pdf_link': news.pdf_link,
+             'resource': news.base_news.news_agency.fa_name,
+             'published date': news.base_news.published_date
+             })
+
+
+@api_view(['POST'])
+@authentication_classes((TokenAuthentication,))
+@permission_classes((IsAuthenticated,))
+def get_telegram_with_entity(request):
+    '''
+    sample body :
+    {
+    "entities":[
+        "نماد وساپا"
+        ]
+    }
+    '''
+    entities = Entity.objects.filter(name__in=request.data['entities'])
+    el_news = news_with_terms(entity_list=entities,
+                              size=50,
+                              start_time='now-10d')
+    try:
+        news_id = [item['_id'] for item in el_news['hits']['hits']]
+    except KeyError:
+        print("Elastic Didn't return Response... !")
+        return False
+    news = News.objects.filter(id__in=news_id, base_news__source_type=3)
+    response = []
+    for n in news:
+        response.append(news_serializer(n))
+    print('finished')
+    return Response(response)
+
--- ChromeExtension/urls.py
+++ ChromeExtension/urls.py
@@ -2,4 +2,8 @@ from django.conf.urls import url, patterns
 
 urlpatterns = [
     url(r'^news/(?P<username>\w+)', 'ChromeExtension.main.chrome_extension_response'),
+    url(r'^telegram_codalir/', 'ChromeExtension.main.get_telegram_codal'),
+    url(r'^telegram_search/', 'ChromeExtension.main.get_telegram_with_entity'),
+
+
 ]
